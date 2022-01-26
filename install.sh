35f9463f7920a8770e03c35997b714b087c0a271
Cypress Frankenfeld <iamcypress@gmail.com>
2022-01-26 00:56:45 -0500

    Add iterm2 preferences

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -s ${BASEDIR}/fish ~/.config
 ln -s ${BASEDIR}/git ~/.config
+
+# Tell iTerm2 to use load its settings from our saved profile
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${BASEDIR}/iterm2"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
