bbff6788f7886cd3715f00c27bab11e970362d9d
Cypress <cypress@lumafield.com>
2024-06-06 23:29:19 -0400

    Add waybar

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s ${BASEDIR}/fish ~/.config
 ln -s ${BASEDIR}/git ~/.config
 ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
 ln -s ${BASEDIR}/hypr ~/.config
+ln -s ${BASEDIR}/waybar ~/.config
 
 # Tell iTerm2 to use load its settings from our saved profile
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${BASEDIR}/iterm2"
