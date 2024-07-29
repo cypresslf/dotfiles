0b36cdef9b42583744147bcde27d4e9e9c2b0ef4
Cypress <cypress@lumafield.com>
2024-07-29 10:41:12 -0400

    Add foot config

--- install.sh
+++ install.sh
@@ -2,12 +2,8 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -s ${BASEDIR}/fish ~/.config
 ln -s ${BASEDIR}/git ~/.config
 ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
 ln -s ${BASEDIR}/hypr ~/.config
 ln -s ${BASEDIR}/waybar ~/.config
-
-# Tell iTerm2 to use load its settings from our saved profile
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${BASEDIR}/iterm2"
-defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+ln -s ${BASEDIR}/foot ~/.config
