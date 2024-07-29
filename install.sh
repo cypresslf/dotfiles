f0fc5d4d53db7dd26c27b1bb68faa0971295b430
Cypress <cypress@lumafield.com>
2024-07-29 10:47:45 -0400

    Remove mac-specific settings

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -s ${BASEDIR}/git ~/.config
-ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
 ln -s ${BASEDIR}/hypr ~/.config
 ln -s ${BASEDIR}/waybar ~/.config
 ln -s ${BASEDIR}/foot ~/.config
