5673ab662d2363ead48fd5a1af7ea6add7cc58e6
Cypress <cypress@lumafield.com>
2025-07-28 15:49:45 -0400

    Make volume adjustment easier
    - Do not exceed 33% input volume because past that the mic starts adjusting ALSA boost which makes it sound clipped and bad
    - Make the volume adjust slower when scrolling via touchpad so it's not 0 to 100% volume with a small scroll

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -s ${BASEDIR}/git ~/.config
 ln -s ${BASEDIR}/hypr ~/.config
+ln -s ${BASEDIR}/sway ~/.config
 ln -s ${BASEDIR}/waybar ~/.config
 ln -s ${BASEDIR}/foot ~/.config
 ln -s ${BASEDIR}/.zshrc ~/
