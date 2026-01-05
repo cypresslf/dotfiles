deccfdd5ae4248db5f868ecc7d2137fb84c84464
Cypress <cypress@lumafield.com>
2026-01-05 11:46:33 -0500

    Install mako config

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ ln -s ${BASEDIR}/sway ~/.config
 ln -s ${BASEDIR}/waybar ~/.config
 ln -s ${BASEDIR}/foot ~/.config
 ln -s ${BASEDIR}/darkman ~/.config
+ln -s ${BASEDIR}/mako ~/.config
 ln -s ${BASEDIR}/.zshrc ~/
 ln -s ${BASEDIR}/tofi.conf ~/.config
